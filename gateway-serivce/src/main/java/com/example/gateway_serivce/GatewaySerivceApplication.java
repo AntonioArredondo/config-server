214db4aef2265572da488fd6e571411084840fc6
Antonio Arredondo <antonio.arredondo.escoto@gmail.com>
2024-08-12 20:18:26 -0600

    se agrega el gateway

---
+++ gateway-serivce/src/main/java/com/example/gateway_serivce/GatewaySerivceApplication.java
@@ -0,0 +1,13 @@
+package com.example.gateway_serivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaySerivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewaySerivceApplication.class, args);
+	}
+
+}
---
+++ gateway-serivce/src/test/java/com/example/gateway_serivce/GatewaySerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gateway_serivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaySerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
